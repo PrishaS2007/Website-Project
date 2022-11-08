8c7cb900095940c6558b558ecdf3ceb3b303d932
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2022-11-08 14:44:58 -0700

    JS

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// JavaScript Code
+// HTML Variables 
+let answerEl = document.getElementById("answerEl");
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked () {
+// Input 
+let q1 = document.getElementById("q1");
+let q2 = document.getElementById("q2");
+let q3 = document.getElementById("q3");
+let q4 = document.getElementById("q4");
+let q5 = document.getElementById("q5");
+}
